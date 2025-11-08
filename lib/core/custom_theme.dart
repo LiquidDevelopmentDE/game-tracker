a795732da606d9321e8158f00cb1cf53a53f63b6
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-11-08 13:49:08 +0100

    Updated theme

--- lib/core/custom_theme.dart
+++ lib/core/custom_theme.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CustomTheme {
-  static Color primaryColor = const Color(0xFF71C0BB);
-  static Color secondaryColor = const Color(0xFF2A4759);
+  static Color primaryColor = const Color(0xFF7505E4);
+  static Color secondaryColor = const Color(0xFFAFA2FF);
   static Color backgroundColor = const Color(0xFF1A1A1A);
 
-  static AppBarTheme appBarTheme = const AppBarTheme(
-    backgroundColor: Color(0xFF1A1A1A),
+  static AppBarTheme appBarTheme = AppBarTheme(
+    backgroundColor: backgroundColor,
     foregroundColor: Colors.white,
     elevation: 0,
-    titleTextStyle: TextStyle(
+    titleTextStyle: const TextStyle(
       color: Colors.white,
       fontSize: 20,
       fontWeight: FontWeight.bold,
     ),
-    iconTheme: IconThemeData(color: Colors.white),
+    iconTheme: const IconThemeData(color: Colors.white),
   );
 }
