34df9f007b0b710662887037723cbec06479162e
Yannick <69087944+GelbEinhalb@users.noreply.github.com>
2025-11-08 17:49:02 +0100

    add navbar_item.dart

---
+++ lib/presentation/widgets/navbar_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class NavbarItem extends StatefulWidget {
+  const NavbarItem({super.key});
+
+  @override
+  State<NavbarItem> createState() => _NavbarItemState();
+}
+
+class _NavbarItemState extends State<NavbarItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
