b6700bafd9c7441de21eae4c98ba17248b742dd0
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-11-12 12:03:08 +0100

    Added on delete cascade

--- lib/data/db/tables/group_game_table.dart
+++ lib/data/db/tables/group_game_table.dart
@@ -3,8 +3,10 @@ import 'package:game_tracker/data/db/tables/group_table.dart';
 import 'package:game_tracker/data/db/tables/player_table.dart';
 
 class GroupGameTable extends Table {
-  TextColumn get groupId => text().references(PlayerTable, #id)();
-  TextColumn get gameId => text().references(GroupTable, #id)();
+  TextColumn get groupId =>
+      text().references(PlayerTable, #id, onDelete: KeyAction.cascade)();
+  TextColumn get gameId =>
+      text().references(GroupTable, #id, onDelete: KeyAction.cascade)();
 
   @override
   Set<Column<Object>> get primaryKey => {groupId, gameId};
