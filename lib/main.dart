2179331455c2b08b4b451d016cac61fec06bba68
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-11-14 09:01:19 +0100

    Removed test code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game_tracker/core/custom_theme.dart';
 import 'package:game_tracker/data/db/database.dart';
-import 'package:game_tracker/data/dto/game.dart';
-import 'package:game_tracker/data/dto/group.dart';
-import 'package:game_tracker/data/dto/player.dart';
 import 'package:game_tracker/presentation/views/main_menu/custom_navigation_bar.dart';
 import 'package:provider/provider.dart';
 
@@ -22,8 +19,6 @@ class GameTracker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    dbCheck(context);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Game Tracker',
@@ -43,30 +38,5 @@ class GameTracker extends StatelessWidget {
 
       home: const CustomNavigationBar(),
     );
-  }
-
-  Future<void> dbCheck(BuildContext context) async {
-    Player player1 = Player(id: 'p1', name: 'Alice');
-    Player player2 = Player(id: 'p2', name: 'Bob');
-    Player player3 = Player(id: 'p3', name: 'Charlie');
-    Player player4 = Player(id: 'p4', name: 'Diana');
-    Group testgroup = Group(
-      id: 'gr1',
-      name: 'Test Group',
-      members: [player1, player2, player3],
-    );
-    Game testgame = Game(
-      id: 'ga1',
-      name: 'Test Game',
-      winner: player1.id,
-      players: [player4],
-      group: testgroup,
-    );
-
-    final db = Provider.of<AppDatabase>(context, listen: false);
-    //await db.gameDao.addGame(game: testgame);
-    print('Game added: ${testgame.name}');
-    final game = await db.gameDao.getGameById(gameId: testgame.id);
-    print(game.toString());
   }
 }
