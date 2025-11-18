f6ebda7984414ac429c5e45227da508272070d30
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-11-19 00:22:35 +0100

    Changed table column because of importing issues

--- lib/data/db/tables/game_table.dart
+++ lib/data/db/tables/game_table.dart
@@ -1,11 +1,9 @@
 import 'package:drift/drift.dart';
-import 'package:game_tracker/data/db/tables/player_table.dart';
 
 class GameTable extends Table {
   TextColumn get id => text()();
   TextColumn get name => text()();
-  TextColumn get winnerId =>
-      text().references(PlayerTable, #id, onDelete: KeyAction.cascade)();
+  late final winnerId = text().nullable()();
 
   @override
   Set<Column<Object>> get primaryKey => {id};
