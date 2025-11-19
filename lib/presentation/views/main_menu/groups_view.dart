54e1756e79326324205acba4874fd0e12b14e88a
mathiskirchner <mathis.kirchner.mk@gmail.com>
2025-11-19 16:39:05 +0100

    moved create_group_view from subfolder to root

--- lib/presentation/views/main_menu/groups_view.dart
+++ lib/presentation/views/main_menu/groups_view.dart
@@ -3,7 +3,7 @@ import 'package:game_tracker/core/custom_theme.dart';
 import 'package:game_tracker/data/db/database.dart';
 import 'package:game_tracker/data/dto/group.dart';
 import 'package:game_tracker/data/dto/player.dart';
-import 'package:game_tracker/presentation/views/main_menu/create_group/create_group_view.dart';
+import 'package:game_tracker/presentation/views/main_menu/create_group_view.dart';
 import 'package:game_tracker/presentation/widgets/custom_width_button.dart';
 import 'package:game_tracker/presentation/widgets/tiles/group_tile.dart';
 import 'package:game_tracker/presentation/widgets/top_centered_message.dart';
