26fadf5093ee49ef0575fdfe7bba1d23042137fc
mathiskirchner <mathis.kirchner.mk@gmail.com>
2025-11-23 15:22:59 +0100

    add artificial delay to loadPlayerList for skeleton loading

--- lib/presentation/views/main_menu/create_group_view.dart
+++ lib/presentation/views/main_menu/create_group_view.dart
@@ -54,7 +54,11 @@ class _CreateGroupViewState extends State<CreateGroupView> {
   }
 
   void loadPlayerList() {
-    _allPlayersFuture = db.playerDao.getAllPlayers();
+    _allPlayersFuture = Future.delayed(
+      const Duration(milliseconds: 400),
+      () => db.playerDao.getAllPlayers(),
+    );
+    suggestedPlayers = skeletonData;
     _allPlayersFuture.then((loadedPlayers) {
       setState(() {
         loadedPlayers.sort((a, b) => a.name.compareTo(b.name));
