0f987f4c7adbd5328288049380bebf5e0e01cb15
gelbeinhalb <exzitonyw@gmail.com>
2026-01-12 11:34:02 +0100

    move match below ids

--- lib/data/db/tables/match_table.dart
+++ lib/data/db/tables/match_table.dart
@@ -4,11 +4,11 @@ import 'package:game_tracker/data/db/tables/group_table.dart';
 
 class MatchTable extends Table {
   TextColumn get id => text()();
-  TextColumn get name => text().nullable()();
   TextColumn get gameId =>
       text().references(GameTable, #id, onDelete: KeyAction.cascade)();
   TextColumn get groupId =>
       text().references(GroupTable, #id, onDelete: KeyAction.cascade).nullable()(); // Nullable if not part of a group
+  TextColumn get name => text().nullable()();
   TextColumn get notes => text().nullable()();
   DateTimeColumn get createdAt => dateTime()();
 
