189daf76ddd9a2cfbcb3ac8fc3e410d803c73a6a
gelbeinhalb <exzitonyw@gmail.com>
2026-01-12 11:35:57 +0100

    move createdAt below description

--- lib/data/db/tables/player_table.dart
+++ lib/data/db/tables/player_table.dart
@@ -3,8 +3,8 @@ import 'package:drift/drift.dart';
 class PlayerTable extends Table {
   TextColumn get id => text()();
   TextColumn get name => text()();
-  DateTimeColumn get createdAt => dateTime()();
   TextColumn get description => text().nullable()();
+  DateTimeColumn get createdAt => dateTime()();
 
   @override
   Set<Column<Object>> get primaryKey => {id};
