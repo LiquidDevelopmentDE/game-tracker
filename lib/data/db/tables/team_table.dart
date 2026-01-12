9b66e58dc08bc820859313d5f45c3503e81b7f98
gelbeinhalb <exzitonyw@gmail.com>
2026-01-12 11:17:13 +0100

    add team table

---
+++ lib/data/db/tables/team_table.dart
@@ -0,0 +1,9 @@
+import 'package:drift/drift.dart';
+
+class TeamTable extends Table {
+  TextColumn get id => text()();
+  TextColumn get name => text()();
+
+  @override
+  Set<Column<Object>> get primaryKey => {id};
+}
