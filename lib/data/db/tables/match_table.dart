d8abad6fd822c0e16d17557215a5742b95c5b4ee
gelbeinhalb <exzitonyw@gmail.com>
2026-01-12 11:30:37 +0100

    add groupid gameid and notes to match

--- lib/data/db/tables/match_table.dart
+++ lib/data/db/tables/match_table.dart
@@ -1,9 +1,15 @@
 import 'package:drift/drift.dart';
+import 'package:game_tracker/data/db/tables/game_table.dart';
+import 'package:game_tracker/data/db/tables/group_table.dart';
 
 class MatchTable extends Table {
   TextColumn get id => text()();
-  TextColumn get name => text()();
-  late final winnerId = text().nullable()();
+  TextColumn get name => text().nullable()();
+  TextColumn get gameId =>
+      text().references(GameTable, #id, onDelete: KeyAction.cascade)();
+  TextColumn get groupId =>
+      text().references(GroupTable, #id, onDelete: KeyAction.cascade).nullable()(); // Nullable if not part of a group
+  TextColumn get notes => text().nullable()();
   DateTimeColumn get createdAt => dateTime()();
 
   @override
