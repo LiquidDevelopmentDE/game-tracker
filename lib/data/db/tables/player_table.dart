56562b22bb3353f43d1e05d411fde988ef3a8646
gelbeinhalb <exzitonyw@gmail.com>
2026-01-12 11:16:02 +0100

    add description to player

--- lib/data/db/tables/player_table.dart
+++ lib/data/db/tables/player_table.dart
@@ -4,6 +4,7 @@ class PlayerTable extends Table {
   TextColumn get id => text()();
   TextColumn get name => text()();
   DateTimeColumn get createdAt => dateTime()();
+  TextColumn get description => text().nullable()();
 
   @override
   Set<Column<Object>> get primaryKey => {id};
