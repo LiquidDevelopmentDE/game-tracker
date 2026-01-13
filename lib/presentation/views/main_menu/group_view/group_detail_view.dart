016c1ceb6e692333350ee6c80df1254c9b211214
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-01-13 21:38:53 +0100

    add context to mounted check

--- lib/presentation/views/main_menu/group_view/group_detail_view.dart
+++ lib/presentation/views/main_menu/group_view/group_detail_view.dart
@@ -83,7 +83,7 @@ class _GroupDetailViewState extends State<GroupDetailView> {
                 ],
               ),
             ).then((confirmed) async {
-              if (confirmed == true && mounted) {
+              if (confirmed == true && context.mounted) {
                 bool success = await db.groupDao.deleteGroup(groupId: widget.groupToEdit!.id);
                 if (!context.mounted) return;
                 if (success) {
